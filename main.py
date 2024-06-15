65d3dbe47b6b681b2532d45d46320835562bf8af
wmaciek <maciek.wilhelmi@gmail.com>
2024-06-15 13:45:57 +0200

    gitattributes added
    for large files

---
+++ main.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from PIL import Image, ImageDraw
+from pix2pix import Pix2Pix, generate_sequence_sharpened
+import torch
+
+st.markdown("<h1 style='text-align: center; color: #7091E6;'>System wizualizacji terapii naczyniowej Dermogen</h1>", unsafe_allow_html=True)
+
+st.text("\n\n")
+
+st.text("W celu skorzystania z systemu wgraj zdjęcie twarzy.\n"
+        "Następnie wybierz interesujący Cię fragment.")
+
+
+def generate_images(pic):
+    results = generate_sequence_sharpened(pix2pix, pic)
+
+    return results
+
+# Load initial image from file
+initial_image_path = 'data/example_pic.png'
+CHECKPOINT_PATH = "model_checkpoint/Pix2Pix_last_one.ckpt"
+
+try:
+    initial_image = Image.open(initial_image_path)
+except FileNotFoundError:
+    initial_image = None
+
+uploaded_image = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+else:
+    image = initial_image
+
+if image is not None:
+    image_width, image_height = image.size
+
+    st.sidebar.header('Wybierz kadr')
+
+    size = 224
+
+    max_x = max(0, image_width - size)
+    max_y = max(0, image_height - size)
+
+    x = st.sidebar.slider('Przesuń w prawo', 0, max_x, 0)
+    y = st.sidebar.slider('Przesuń w dół', 0, max_y, 0)
+
+    preview_image = image.copy()
+    draw = ImageDraw.Draw(preview_image)
+    draw.rectangle([x, y, x + size, y + size], outline="blue", width=3)
+
+    st.image(preview_image, caption='Obraz z wybranym oknem', use_column_width=True)
+
+    pix2pix = Pix2Pix(3, 3)
+    pix2pix.load_state_dict(torch.load(CHECKPOINT_PATH))
+
+    if st.sidebar.button('Wybierz'):
+        left = x
+        top = y
+        right = x + size
+        bottom = y + size
+
+        cropped_image = image.crop((left, top, right, bottom))
+        # cropped_image = cropped_image.resize((224, 224))
+
+        rotated_images = generate_images(cropped_image)
+
+        modified_images = []
+        for rotated in rotated_images:
+            modified_image = image.copy()
+            modified_image.paste(rotated, (left, top))
+            draw = ImageDraw.Draw(modified_image)
+            draw.rectangle([left, top, right, bottom], outline="blue", width=2)
+            modified_images.append(modified_image)
+
+        st.session_state.modified_images = modified_images
+
+    if 'modified_images' in st.session_state:
+        rotation_idx = st.slider('Wybierz kolejne efekty', 0, 3, 0)
+        st.image(st.session_state.modified_images[rotation_idx],
+                 caption=f'Efekty po {rotation_idx}. zabiegu', use_column_width=True)
+else:
+    st.write("Brak dostępnych obrazów. Wgraj proszę nowe zdjęcie.")
+
---
+++ pix2pix.py
@@ -0,0 +1,270 @@
+# pytorch
+import torch
+import torch.nn as nn
+
+
+# ai libs
+from torchvision import transforms
+import cv2
+import numpy as np
+from PIL import Image
+import pytorch_lightning as pl
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
+
+transform = transforms.Compose([transforms.ToPILImage(), transforms.Resize(256), transforms.ToTensor()])
+
+
+class DownSampleConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel=4, strides=2, padding=1, activation=True, batchnorm=True):
+        """
+        Paper details:
+        - C64-C128-C256-C512-C512-C512-C512-C512
+        - All convolutions are 4×4 spatial filters applied with stride 2
+        - Convolutions in the encoder downsample by a factor of 2
+        """
+        super().__init__()
+        self.activation = activation
+        self.batchnorm = batchnorm
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel, strides, padding)
+
+        if batchnorm:
+            self.bn = nn.BatchNorm2d(out_channels)
+
+        if activation:
+            self.act = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.batchnorm:
+            x = self.bn(x)
+        if self.activation:
+            x = self.act(x)
+        return x
+
+
+class UpSampleConv(nn.Module):
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel=4,
+        strides=2,
+        padding=1,
+        activation=True,
+        batchnorm=True,
+        dropout=False
+    ):
+        super().__init__()
+        self.activation = activation
+        self.batchnorm = batchnorm
+        self.dropout = dropout
+
+        self.deconv = nn.ConvTranspose2d(in_channels, out_channels, kernel, strides, padding)
+
+        if batchnorm:
+            self.bn = nn.BatchNorm2d(out_channels)
+
+        if activation:
+            self.act = nn.ReLU(True)
+
+        if dropout:
+            self.drop = nn.Dropout2d(0.5)
+
+    def forward(self, x):
+        x = self.deconv(x)
+        if self.batchnorm:
+            x = self.bn(x)
+
+        if self.dropout:
+            x = self.drop(x)
+        return x
+
+
+class Generator(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        """
+        Paper details:
+        - Encoder: C64-C128-C256-C512-C512-C512-C512-C512
+        - All convolutions are 4×4 spatial filters applied with stride 2
+        - Convolutions in the encoder downsample by a factor of 2
+        - Decoder: CD512-CD1024-CD1024-C1024-C1024-C512 -C256-C128
+        """
+        super().__init__()
+
+        # encoder/donwsample convs
+        self.encoders = [
+            DownSampleConv(in_channels, 64, batchnorm=False),  # bs x 64 x 128 x 128
+            DownSampleConv(64, 128),  # bs x 128 x 64 x 64
+            DownSampleConv(128, 256),  # bs x 256 x 32 x 32
+            DownSampleConv(256, 512),  # bs x 512 x 16 x 16
+            DownSampleConv(512, 512),  # bs x 512 x 8 x 8
+            DownSampleConv(512, 512),  # bs x 512 x 4 x 4
+            DownSampleConv(512, 512),  # bs x 512 x 2 x 2
+            DownSampleConv(512, 512, batchnorm=False),  # bs x 512 x 1 x 1
+        ]
+
+        # decoder/upsample convs
+        self.decoders = [
+            UpSampleConv(512, 512, dropout=True),  # bs x 512 x 2 x 2
+            UpSampleConv(1024, 512, dropout=True),  # bs x 512 x 4 x 4
+            UpSampleConv(1024, 512, dropout=True),  # bs x 512 x 8 x 8
+            UpSampleConv(1024, 512),  # bs x 512 x 16 x 16
+            UpSampleConv(1024, 256),  # bs x 256 x 32 x 32
+            UpSampleConv(512, 128),  # bs x 128 x 64 x 64
+            UpSampleConv(256, 64),  # bs x 64 x 128 x 128
+        ]
+        self.decoder_channels = [512, 512, 512, 512, 256, 128, 64]
+        self.final_conv = nn.ConvTranspose2d(64, out_channels, kernel_size=4, stride=2, padding=1)
+        self.tanh = nn.Tanh()
+
+        self.encoders = nn.ModuleList(self.encoders)
+        self.decoders = nn.ModuleList(self.decoders)
+
+    def forward(self, x):
+        skips_cons = []
+        for encoder in self.encoders:
+            x = encoder(x)
+
+            skips_cons.append(x)
+
+        skips_cons = list(reversed(skips_cons[:-1]))
+        decoders = self.decoders[:-1]
+
+        for decoder, skip in zip(decoders, skips_cons):
+            x = decoder(x)
+            # print(x.shape, skip.shape)
+            x = torch.cat((x, skip), axis=1)
+
+        x = self.decoders[-1](x)
+        # print(x.shape)
+        x = self.final_conv(x)
+        return self.tanh(x)
+
+
+class PatchGAN(nn.Module):
+
+    def __init__(self, input_channels):
+        super().__init__()
+        self.d1 = DownSampleConv(input_channels, 64, batchnorm=False)
+        self.d2 = DownSampleConv(64, 128)
+        self.d3 = DownSampleConv(128, 256)
+        self.d4 = DownSampleConv(256, 512)
+        self.final = nn.Conv2d(512, 1, kernel_size=1)
+
+    def forward(self, x, y):
+        x = torch.cat([x, y], axis=1)
+        x0 = self.d1(x)
+        x1 = self.d2(x0)
+        x2 = self.d3(x1)
+        x3 = self.d4(x2)
+        xn = self.final(x3)
+        return xn
+
+
+class Pix2Pix(pl.LightningModule):
+
+    def __init__(self, in_channels, out_channels, learning_rate=0.0002, lambda_recon=200):
+
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.gen = Generator(in_channels, out_channels)
+        self.patch_gan = PatchGAN(in_channels + out_channels)
+
+        # intializing weights
+        #self.gen = self.gen.apply(_weights_init)
+        #self.patch_gan = self.patch_gan.apply(_weights_init)
+
+        self.adversarial_criterion = nn.BCEWithLogitsLoss()
+        self.recon_criterion = nn.L1Loss()
+        self.automatic_optimization = False
+
+    def _gen_step(self, real_images, conditioned_images):
+        # Pix2Pix has adversarial and a reconstruction loss
+        # First calculate the adversarial loss
+        fake_images = self.gen(conditioned_images)
+        disc_logits = self.patch_gan(fake_images, conditioned_images)
+        adversarial_loss = self.adversarial_criterion(disc_logits, torch.ones_like(disc_logits))
+
+        # calculate reconstruction loss
+        recon_loss = self.recon_criterion(fake_images, real_images)
+        lambda_recon = self.hparams.lambda_recon
+
+        return adversarial_loss + lambda_recon * recon_loss
+
+    def _disc_step(self, real_images, conditioned_images):
+        fake_images = self.gen(conditioned_images).detach()
+        fake_logits = self.patch_gan(fake_images, conditioned_images)
+
+        real_logits = self.patch_gan(real_images, conditioned_images)
+
+        fake_loss = self.adversarial_criterion(fake_logits, torch.zeros_like(fake_logits))
+        real_loss = self.adversarial_criterion(real_logits, torch.ones_like(real_logits))
+
+        return (real_loss + fake_loss) / 2
+
+    def configure_optimizers(self):
+        lr = self.hparams.learning_rate
+        gen_opt = torch.optim.Adam(self.gen.parameters(), lr=lr)
+        disc_opt = torch.optim.Adam(self.patch_gan.parameters(), lr=lr)
+
+        return disc_opt, gen_opt
+
+    def training_step(self, batch, batch_idx):
+        real, condition = batch
+        real, condition = real.to(device), condition.to(device)
+        disc_opt, gen_opt = self.optimizers()
+
+        disc_loss = self._disc_step(real, condition)
+        self.log('PatchGAN Loss', disc_loss)
+
+        disc_opt.zero_grad()
+        self.manual_backward(disc_loss)
+        disc_opt.step()
+
+        gen_loss = self._gen_step(real, condition)
+        self.log('Generator Loss', gen_loss)
+        gen_opt.zero_grad()
+        self.manual_backward(gen_loss)
+        gen_opt.step()
+
+
+def sharpen_image_pil(image_tensor):
+    image = transforms.ToPILImage()(image_tensor.cpu())
+    image_np = np.array(image)
+
+    # Tworzenie kernela wyostrzającego
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5,-1],
+                       [0, -1, 0]])
+
+    sharpened_image_np = cv2.filter2D(image_np, -1, kernel)
+    sharpened_image = Image.fromarray(sharpened_image_np)
+
+    return transforms.ToTensor()(sharpened_image).to(device)
+
+
+def generate_sequence_sharpened(model, img, sequence_len=3):
+    results = []
+    model.eval()
+    model = model.to(device)
+
+    # Ensure the initial image is copied to avoid negative stride issues
+    current_img = transform(img.copy()).unsqueeze(0).to(device)
+    for i in range(1, sequence_len):
+        current_img = model.gen(current_img)
+        # Ensure current_img is contiguous before further processing
+        current_img = current_img.contiguous()
+        # Apply sharpening and ensure the result is also contiguous
+        current_img = sharpen_image_pil(current_img[0])
+        # Add batch dimension back if it was removed
+        current_img = current_img.unsqueeze(0)
+        results.append(current_img[0].resize((224, 224)))
+
+    return results
